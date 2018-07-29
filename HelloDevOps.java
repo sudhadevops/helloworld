1d2d2b2c83e4e962228f8b935cf1b3a68a401975
sudha <sudharani.daddala@gmail.com>
2018-07-29 18:58:02 -0500

    HelloWorld & HelloDevOps files

---
+++ HelloDevOps.java
@@ -0,0 +1,11 @@
+public class HelloDevOps {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+
+        System.out.println("******************************");
+        System.out.println("Welcome to the world of DevOps");
+        System.out.println("******************************");
+    }
+
+}
